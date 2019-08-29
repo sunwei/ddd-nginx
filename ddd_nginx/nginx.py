c1eecb4cea35e4cca0f56d75b643c4b2a0415eb3
sunwei <me@sunwei.xyz>
2019-08-29 21:12:24 +0800

    Modified: refactor dumps; Wayde

--- ddd_nginx/nginx.py
+++ ddd_nginx/nginx.py
@@ -30,56 +30,71 @@ class Nginx(Aggregate, Block):
     def namespace(self, value):
         self._namespace = value + '.' + self.host
 
-    def _dump_map(self, directory):
-        for idx, the_map in enumerate(self.maps):
-            a_map = the_map.dump("map.conf.jinja2", {
+    def _dump_map(self):
+        def map_dump(the_map):
+            return the_map.dump("map.conf.jinja2", {
                 "key_name": the_map.definition.key,
                 "value_name": the_map.definition.value,
                 "key_pairs": the_map.key_pairs
             })
-            create_file(os.path.join(directory, str(idx) + "_map.conf"), a_map)
 
-    def _dump_upstream(self, directory):
-        for the_upstream in self.upstreams:
-            a_upstream = the_upstream.dump("upstream.conf.jinja2", {
+        return [map_dump(a_map) for a_map in self.maps]
+
+    def _dump_upstream(self):
+        def map_upstream(the_upstream):
+            return the_upstream.dump("upstream.conf.jinja2", {
                 "name": the_upstream.name,
                 "servers": the_upstream.servers,
             })
-            create_file(os.path.join(directory, the_upstream.name + "_upstream.conf"), a_upstream)
 
-    def _dump_location(self, directory):
-        location_dir = os.path.join(directory, "locations_conf.d")
-        make_dir(location_dir)
-        for idx, the_location in enumerate(self.locations):
-            a_location = the_location.dump("location.conf.jinja2", {
+        return [map_upstream(a_upstream) for a_upstream in self.upstreams]
+
+    def _dump_location(self):
+        def map_location(the_location):
+            return the_location.dump("location.conf.jinja2", {
                 "name": the_location.name,
                 "proxy": the_location.proxy,
                 "variables": the_location.sets,
                 "scope": the_location.scope
             })
-            create_file(os.path.join(location_dir, str(idx) + "_location.conf"), a_location)
 
-    def _dump_server(self, directory):
-        for idx, the_server in enumerate(self.servers):
-            a_server = the_server.dump("server.conf.jinja2", {
+        return [map_location(a_location) for a_location in self.locations]
+
+    def _dump_server(self):
+        def map_server(the_server):
+            return the_server.dump("server.conf.jinja2", {
                 "name": the_server.name,
                 "variables": the_server.sets,
             })
-            create_file(os.path.join(directory, str(idx) + "_server.conf"), a_server)
 
-    def _dump_nginx(self, directory):
-        a_nginx = self.dump("nginx.conf.jinja2", {
+        return [map_server(a_server) for a_server in self.servers]
+
+    def _dump_nginx(self):
+        return self.dump("nginx.conf.jinja2", {
             "namespace": self.namespace,
         })
-        create_file(os.path.join(directory, "root.conf"), a_nginx)
+
+    @staticmethod
+    def _generate_conf(directory, configs, name):
+        for idx, a_config in enumerate(configs):
+            create_file(os.path.join(directory, str(idx) + '_' + name + '.conf'), a_config)
 
     def dumps(self, directory):
         make_dir(directory)
-        self._dump_map(directory)
-        self._dump_upstream(directory)
-        self._dump_location(directory)
-        self._dump_server(directory)
-        self._dump_nginx(directory)
+        maps = self._dump_map()
+        upstreams = self._dump_upstream()
+        locations = self._dump_location()
+        servers = self._dump_server()
+        nginx = self._dump_nginx()
+
+        self._generate_conf(directory, maps, 'map')
+        self._generate_conf(directory, upstreams, 'upstream')
+        location_dir = os.path.join(directory, "locations_conf.d")
+        make_dir(location_dir)
+        self._generate_conf(location_dir, locations, 'location')
+        self._generate_conf(directory, servers, 'server')
+        create_file(os.path.join(directory, "root.conf"), nginx)
+
         copy_file(
             os.path.join(TEMPLATE_DIR, "error_page.conf"),
             os.path.join(directory, "error_page.conf"))
